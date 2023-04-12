fa13647aa189086201c556739cc3c4d1cb3ffd1d
张文军 <zhangwenjun02@tojoy.com>
2023-04-12 17:42:23 +0800

    nvue

---
+++ BJUniAppDemo/pages/layout/layout.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="layout">
+		<view v-for="(title, index) in list" class="layout-list" :key="index">
+			<view class="layout-list-item" @click="clickItem">
+				{{ title }}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ["nvue"]
+			}
+		},
+		methods: {
+			clickItem() {
+				console.log("click");
+				uni.navigateTo({
+					url: '/pages/layout/nvuePage'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
